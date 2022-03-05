b67e201cd9b8a104d076684dd6a21c4f6e3e0373
小沈曰 <409178030@qq.com>
2022-03-05 22:08:48 +0800

    vant

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <div id="app">
     <h1>app</h1>
     <i class="toutiao toutiao-dianzan"></i>
+    <hr />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述内容" />
+    </van-cell-group>
     <router-view></router-view>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.less'
 
 Vue.config.productionTip = false
+Vue.use(Vant)
 
 new Vue({
   router,
