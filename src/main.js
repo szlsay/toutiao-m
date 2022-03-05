334e173f7c3eae88820b7f3746c2f1a1d92287bc
小沈曰 <409178030@qq.com>
2022-03-05 22:15:35 +0800

    amfe-flexible

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 
+import 'amfe-flexible'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
