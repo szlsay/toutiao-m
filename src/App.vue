00702854c19bde6a4e4941f0aaa2140aae3dc573
小沈曰 <409178030@qq.com>
2022-03-06 11:09:56 +0800

    sms

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <div id="app">
-    <h1>app</h1>
-    <i class="toutiao toutiao-dianzan"></i>
-    <hr />
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-cell-group>
-      <van-cell title="单元格" value="内容" />
-      <van-cell title="单元格" value="内容" label="描述内容" />
-    </van-cell-group>
-    <div class="font1">Test</div>
     <router-view></router-view>
   </div>
 </template>
